4a33686fb898e18c601d39008db138dcd32288ef
Grzegorz <grze.pra@gmail.com>
2025-04-09 13:17:22 +0200

    Almost done

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,43 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { User } from '../../types/User';
+import { Comment } from '../../types/Comment';
 
-export const PostInfo: React.FC = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+import './PostInfo.scss';
+
+interface Props {
+  post: Post;
+  users: User[];
+  comments: Comment[];
+}
+
+export const PostInfo: React.FC<Props> = ({ post, users, comments }) => {
+  const postUser = users.find(user => post.userId === user.id);
+  const postComments = comments.filter(comment => post.id === comment.postId);
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {` Posted by  `}
+
+          {postUser && <UserInfo user={postUser} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+      {postComments.length > 0 ? (
+        <CommentList comments={postComments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,20 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => <>Put the list here</>;
+import { Post } from '../../types/Post';
+import { User } from '../../types/User';
+import { Comment } from '../../types/Comment';
+
+interface Props {
+  posts: Post[];
+  users: User[];
+  comments: Comment[];
+}
+
+export const PostList: React.FC<Props> = ({ posts, users, comments }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} users={users} comments={comments} key={post.id} />
+    ))}
+  </div>
+);
